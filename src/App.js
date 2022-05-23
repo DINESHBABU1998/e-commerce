a158db6596e88e3ac1516a22a8979bc9f1919b68
DINESHBABU1998 <dineshbabut25@gmail.com>
2022-05-23 19:16:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Student from './components/Student';
 import Contactus from './components/Contactus';
 import Nav from './components/Nav';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
