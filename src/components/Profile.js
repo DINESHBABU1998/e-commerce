de8cf6b985332a04e47a133f6cc8a475a184de9d
T DINESH BABU <98023477+DINESHBABU1998@users.noreply.github.com>
2022-07-09 15:56:00 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,11 @@ function Profile() {
     <div>
       <form>
         <label className='sign'>SIGN IN / LOGIN IN</label>
+        <input type='text' placeholder='name'/><br/><br/>
+        <input type='email' placeholder='email'/><br/><br/>
+        <input type='password' placeholder='password'/><br/><br/>
+        <input type='number' placeholder='number'/><br/><br/>
+        <input type='submit' value='Buy Now'/>
       </form>
     </div>
   )
