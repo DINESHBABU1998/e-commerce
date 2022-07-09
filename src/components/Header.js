f09e63c7d2c8fe79b5655af9b61acb1c4c92e8ec
T DINESH BABU <98023477+DINESHBABU1998@users.noreply.github.com>
2022-07-09 15:31:39 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,17 @@ import './style.css'
 function Header() {
 return (
       <div>
-          <h1 className='heading'>iSHOP</h1>
+          <h1 className='heading'>Bella</h1>
           <div className='nav-box' >
-          <NavLink className="text" to='/' style={ ( {isActive} ) => { return { color: isActive? 'red' : 'blue'} } }><i class="fa-solid fa-house"></i>Home</NavLink>
-          <NavLink className="text" to='/fashion'style={ ( {isActive} ) => { return { color: isActive? 'red' : 'blue'} } }><i class="fa-solid fa-shirt"></i>Fashion</NavLink>
-          <NavLink className="text" to='/laptops'style={ ( {isActive} ) => { return { color: isActive? 'red' : 'blue'} } }><i class="fa-solid fa-laptop"></i>Laptops</NavLink>
-          <NavLink className="text" to='/mobiles'style={ ( {isActive} ) => { return { color: isActive? 'red' : 'blue'} } }><i class="fa-solid fa-mobile"></i>Mobiles</NavLink>
-          <NavLink className="text" to='/accesories'style={ ( {isActive} ) => { return { color: isActive? 'red' : 'blue'} } }><i class="fa-solid fa-headphones"></i>Accesories</NavLink> 
+          <NavLink className="text" to='/'><i class="fa-solid fa-house"></i>Home</NavLink>
+          <NavLink className="text" to='/fashion'><i class="fa-solid fa-shirt"></i>Fashion</NavLink>
+          <NavLink className="text" to='/laptops'><i class="fa-solid fa-laptop"></i>Laptops</NavLink>
+          <NavLink className="text" to='/mobiles'><i class="fa-solid fa-mobile"></i>Mobiles</NavLink>
+          <NavLink className="text" to='/accesories'><i class="fa-solid fa-headphones"></i>Accesories</NavLink> 
          </div>
          <div>
-          <NavLink className="text1" to='/profile' style={ ( {isActive} ) => { return { color: isActive? 'blue' : 'black'} } }><i class="fa-solid fa-user"></i>My Profile</NavLink>
-          <NavLink className="text2" to='/addcart' style={ ( {isActive} ) => { return { color: isActive? 'blue' : 'black'} } }><i class="fa-solid fa-cart-arrow-down"></i>Cart</NavLink>
+          <NavLink className="text1" to='/profile'><i class="fa-solid fa-user"></i>My Profile</NavLink>
+          <NavLink className="text2" to='/addcart'><i class="fa-solid fa-cart-arrow-down"></i>Cart</NavLink>
           <form>
             <input className='search' type='search' placeholder='Search'/>
           </form>
